27687ee1b08fe87f36a6eab159e20a3ce974fd6b
Keegan Lawley <keeganlk.7@gmail.com>
2018-10-23 14:38:20 -0500

    general layout

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 800, height: 600, titleBarStyle: 'hidden'})
 
   // and load the index.html of the app.
   win.loadFile('index.html')
